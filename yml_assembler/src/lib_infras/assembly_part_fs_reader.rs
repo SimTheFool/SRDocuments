733f8420035d3160ee214fff386f5a50d3c86a15
SimTheFool <simon.sauzede@gmail.com>
2023-09-30 15:00:31 +0200

    feat: cache reading ok

--- yml_assembler/src/lib_infras/assembly_part_fs_reader.rs
+++ yml_assembler/src/lib_infras/assembly_part_fs_reader.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{cell::RefCell, collections::HashMap, path::PathBuf};
 
 use crate::{
     adapters::PartReaderPort,
@@ -7,18 +7,35 @@ use crate::{
 
 pub struct PartFSReader {
     context: PathBuf,
+    cache: RefCell<HashMap<String, serde_yaml::Value>>,
 }
 impl PartFSReader {
     pub fn new(path: PathBuf) -> Self {
-        PartFSReader { context: path }
+        PartFSReader {
+            context: path,
+            cache: RefCell::new(HashMap::new()),
+        }
     }
 }
 impl PartReaderPort for PartFSReader {
     fn get_value(&self, identifier: &str) -> AppResult<serde_yaml::Value> {
-        let path = self.context.join(format!("{identifier}.pyml"));
-        println!("loading: {:?}", path);
-        let file = std::fs::File::open(path).map_err(AppError::other)?;
-        let yml: serde_yaml::Value = serde_yaml::from_reader(file).map_err(AppError::other)?;
-        Ok(yml)
+        let mut cache = self.cache.borrow_mut();
+        let cached_value = cache.get(identifier);
+
+        match cached_value {
+            Some(value) => {
+                println!("reading from cache: {}", identifier);
+                Ok(value.clone())
+            }
+            None => {
+                let path = self.context.join(format!("{identifier}.pyml"));
+                println!("reading: {}", identifier);
+                let file = std::fs::File::open(path).map_err(AppError::other)?;
+                let yml: serde_yaml::Value =
+                    serde_yaml::from_reader(file).map_err(AppError::other)?;
+                cache.insert(identifier.to_string(), yml.clone());
+                Ok(yml)
+            }
+        }
     }
 }
